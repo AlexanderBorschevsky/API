c6922a4af4f76ca59c1e101336d75df16c3c5407
AlexanderBorschevsky <alexanderborschevsky0411@gmail.com>
2023-11-03 23:29:58 +0300

    firstcommit

--- chess/chessbackend/models.py
+++ chess/chessbackend/models.py
@@ -1,14 +1,20 @@
+from datetime import date
+
 from django.db import models
 from django.contrib.auth.hashers import make_password
+from django.utils import timezone
 
 # Create your models here.
 from django.db import models
 
+
 class MyUser(models.Model):
-    name = models.CharField(max_length=255)
-    email = models.EmailField(max_length=500, unique=True)
-    username = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(max_length=70, unique=True)
+    login = models.CharField(max_length=20, unique=True, null=True)
     password = models.CharField(max_length=255)
+    # custom_id = models.AutoField(primary_key=True)
+    registration_date = models.DateField(default=date.today)
+    # achievments = models.
 
     def save(self, *args, **kwargs):
         # Хешируем пароль перед сохранением
