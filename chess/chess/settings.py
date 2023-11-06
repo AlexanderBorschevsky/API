51f2505b07a0a419c0ff6a97c2cfd792c5e93af8
AlexanderBorschevsky <alexanderborschevsky0411@gmail.com>
2023-11-06 13:50:03 +0300

    исключил sh

--- chess/chess/settings.py
+++ chess/chess/settings.py
@@ -144,7 +144,6 @@ SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
 STATICFILES_DIRS = [BASE_DIR/'static',]
-STATIC_ROOT = BASE_DIR/'staticfiles'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build' , 'static' )
 MEDIA_URLS = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media' )
