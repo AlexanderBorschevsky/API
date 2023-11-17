59f06d315218438f603595ba98e9d7cf31c2b57b
AlexanderBorschevsky <alexanderborschevsky0411@gmail.com>
2023-11-17 17:05:04 +0300

    бд

--- chess/chess/settings.py
+++ chess/chess/settings.py
@@ -80,11 +80,14 @@ WSGI_APPLICATION = 'chess.wsgi.app'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'qwerty',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
