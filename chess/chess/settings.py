ef4c9839e49c2a40b79c078445b7828e31341155
AlexanderBorschevsky <alexanderborschevsky0411@gmail.com>
2023-11-17 21:26:10 +0300

    debug

--- chess/chess/settings.py
+++ chess/chess/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1io+rne-_38g_q$!)m*@^vo5w#mq%%^w+t%^=ydejh*mn0n=@g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
