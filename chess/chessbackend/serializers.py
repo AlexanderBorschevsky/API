83a0dc313ae50af013a9abfaee4e365b919d844e
AlexanderBorschevsky <alexanderborschevsky0411@gmail.com>
2023-11-18 16:29:31 +0300

    сделал проверку на длину пароля

--- chess/chessbackend/serializers.py
+++ chess/chessbackend/serializers.py
@@ -1,3 +1,5 @@
+import re
+
 from django.utils.crypto import get_random_string
 from rest_framework import serializers
 
@@ -12,6 +14,14 @@ class MyUserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         password = validated_data.pop('password', None)
+        if not re.search(r'\d', password) or \
+                not re.search(r'[a-z]', password) or \
+                not re.search(r'[A-Z]', password) or \
+                not re.search(r'[!@#$%^&*(),.?":{}|<>]', password) or\
+                len(password)<8:
+                    raise serializers.ValidationError(
+                        "Пароль должен быть не менее 8 символов, содержать хотя бы одну цифру, одну строчную и прописную буквы, а также один из следующих символов: !@#$%^&*(),.?\":{}|<>"
+                    )
         instance = self.Meta.model(**validated_data)
         if password is not None:
             instance.set_password(password)
