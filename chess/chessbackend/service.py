d546c2f579914f47dcca14c4e062b5426af9b4fb
AlexanderBorschevsky <alexanderborschevsky0411@gmail.com>
2024-02-02 16:15:19 +0300

    добавлена фича по сбросу пароля

--- chess/chessbackend/service.py
+++ chess/chessbackend/service.py
@@ -1,7 +1,7 @@
 from django.core.mail import send_mail
 from rest_framework.reverse import reverse
 
-from chess import settings
+BASE_URL = 'https://shiferchess.ru'
 
 
 class EmailConfirmationService:
@@ -9,8 +9,21 @@ class EmailConfirmationService:
     def send_registration_email(email, confirmation_token):
         subject = 'Регистрация успешна'
         confirmation_url = reverse('confirm-registration', args=[confirmation_token])
-        full_confirm_url = settings.BASE_URL + confirmation_url
-        message = f'Спасибо за регистрацию на нашем сайте. Ваш аккаунт успешно создан. Пожалуйста, подтвердите его, перейдя по ссылке: {full_confirm_url }'
+        full_confirm_url = BASE_URL + confirmation_url
+        message = f'Спасибо за регистрацию на нашем сайте. Ваш аккаунт успешно создан. Пожалуйста, подтвердите его, перейдя по ссылке: {full_confirm_url}'
+        from_email = 'enjoychess@yandex.ru'
+        recipient_list = [email]
+
+        send_mail(subject, message, from_email, recipient_list)
+
+
+class PasswordResetService:
+    @staticmethod
+    def reset_password(email, confirmation_token):
+        subject = 'Сброс пароля'
+        reset_url = reverse('password-reset', args=[confirmation_token])
+        full_reset_url = BASE_URL + reset_url
+        message = f'Для сброса пароля перейдите по ссылке: {full_reset_url}'
         from_email = 'enjoychess@yandex.ru'
         recipient_list = [email]
 
--- chess/chessbackend/views.py
+++ chess/chessbackend/views.py
@@ -1,9 +1,11 @@
+import re
 import secrets
+from django.contrib.auth.hashers import make_password
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from .service import EmailConfirmationService
+from .service import EmailConfirmationService, PasswordResetService
 from .serializers import MyUserSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -11,13 +13,15 @@ from rest_framework import status
 from django.http import JsonResponse, HttpResponse
 from rest_framework_simplejwt.tokens import RefreshToken
 from .models import MyUser
+
+
 def index(request):
-   return HttpResponse('Сайт работает!')
+    return HttpResponse('Сайт работает!')
+
 
 # Create your views here.
 class MyUserAPIList(APIView):
     def generate_confirmation_token(self):
-        # Генерация уникального токена
         return secrets.token_urlsafe(16)
 
     def post(self, request, *args, **kwargs):
@@ -34,12 +38,10 @@ class MyUserAPIList(APIView):
 
 class ConfirmRegistrationView(APIView):
     def get(self, request, confirmation_token):
-
         user = get_object_or_404(MyUser, confirmation_token=confirmation_token)
-
         user.email_confirmed = True
+        user.confirmation_token = None
         user.save()
-
         return Response({'message': 'Регистрация подтверждена успешно.'}, status=status.HTTP_200_OK)
 
 
@@ -52,26 +54,20 @@ class Login(APIView):
             user = MyUser.objects.filter(email=email).first()
         elif login:
             user = MyUser.objects.filter(login=login).first()
-
         if user is None:
             return Response({'message': 'Пользователь не найден'}, status=400)
-
-        if not user.is_email_confirmed():
+        if user.email_confirmed is False:
             return Response({'message': 'Подтвердите почту'}, status=400)
-
         if not user.check_password(password):
             return Response({'message': 'Неверный пароль'}, status=400)
         refresh = (RefreshToken.for_user(user))
         access_token = str(refresh.access_token)
         refresh_token = str(refresh)
-
-
         response = Response(
             {'message': 'Пользователь прошел проверку', 'access_token': str(access_token), 'refresh': str(refresh),
              'login': user.login})
         response.set_cookie('refresh_token', (refresh_token), max_age=refresh.lifetime.total_seconds(),
                             httponly=True, samesite='None', secure=True)
-
         return response
 
 
@@ -81,18 +77,12 @@ class UserLogin(APIView):
         refresh_token = request.COOKIES.get('refresh_token')
         refresh_token = RefreshToken(refresh_token)
         user_id = refresh_token.payload.get('user_id')
-
         try:
             user = MyUser.objects.get(id=user_id)
-
             if new_login:
                 user.login = new_login
                 user.save()
-
-            # Другие операции, если нужны
-
             return Response({'message': 'Логин успешно обновлен', 'login': user.login}, status=status.HTTP_200_OK)
-
         except MyUser.DoesNotExist:
             return Response({'error': 'Пользователь не найден'}, status=status.HTTP_404_NOT_FOUND)
         except Exception as e:
@@ -105,7 +95,6 @@ class HelloWorldView(APIView):
 
     def get(self, request):
         user = request.user
-
         return Response({'message': 'Hello, World!'})
 
 
@@ -140,7 +129,6 @@ def refresh_access_token(request):
                 return response
             except Exception as e:
                 return JsonResponse({'error': str(e)}, status=400)
-
     return JsonResponse({'detail': 'Invalid request method'}, status=400)
 
 
@@ -148,7 +136,6 @@ class AuthUser(APIView):
     def post(self, request):
         try:
             refresh_token_value = request.COOKIES.get('refresh_token')
-
             if refresh_token_value:
                 refresh_token = RefreshToken(refresh_token_value)
                 access_token = str(refresh_token.access_token)
@@ -156,7 +143,6 @@ class AuthUser(APIView):
                 user = MyUser.objects.get(id=user_id)
                 user_email = user.email
                 user_login = user.login
-
                 new_refresh_token = RefreshToken.for_user(user)
                 response_data = {
                     'access_token': access_token,
@@ -173,8 +159,47 @@ class AuthUser(APIView):
                     secure=True,
                 )
                 return response
-
         except MyUser.DoesNotExist:
             return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+class ResetPassword(APIView):
+    def generate_confirmation_token(self):
+        return secrets.token_urlsafe(16)
+
+    def post(self, request):
+        user_email = request.data.get('email')
+        if not user_email:
+            return Response({'error': 'User_email not found'}, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            user = MyUser.objects.get(email=user_email)
+            user.confirmation_token = self.generate_confirmation_token()
+            user.save()
+        except MyUser.DoesNotExist:
+            return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
+        PasswordResetService.reset_password(user.email, user.confirmation_token)
+        return Response({
+            'message': 'Для сброса пароля, перейдите на указанную почту .',
+            'confirmation_token': user.confirmation_token
+        }, status=status.HTTP_201_CREATED)
+
+
+class ResetConfirmPassword(APIView):
+    def post(self, request, confirmation_token):
+        password=request.data.get('password')
+        user = get_object_or_404(MyUser, confirmation_token=confirmation_token)
+        if not re.search(r'\d', password) or \
+                not re.search(r'[a-z]', password) or \
+                not re.search(r'[A-Z]', password) or \
+                not re.search(r'[@.#$!%*_?&^]', password) or \
+                len(password) < 8:
+            return Response(
+                "Пароль должен быть не менее 8 символов, содержать хотя бы одну цифру, одну строчную и прописную буквы, а также один из следующих символов: @.#$!%*_?&^"
+            )
+        else:
+            user.password = make_password(password)
+            user.confirmation_token=None
+            user.save()
+            return Response({'message': 'Поздравляем, ваш пароль успешно обновлен.'}, status=status.HTTP_200_OK)
