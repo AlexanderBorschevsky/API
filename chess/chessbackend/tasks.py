14a565bb97dcecf4ad5c45dd842bc01e84b930db
AlexanderBorschevsky <alexanderborschevsky0411@gmail.com>
2024-02-13 22:06:44 +0300

    редис

--- chess/chessbackend/tasks.py
+++ chess/chessbackend/tasks.py
@@ -3,6 +3,6 @@ from celery import shared_task
 from .service import EmailConfirmationService  # Подставьте свой путь к сервису
 from .serializers import MyUserSerializer  # Подставьте свой путь к сериализатору
 
-@shared_task
+@shared_task()
 def send_registration_email_task(email, confirmation_token):
     EmailConfirmationService.send_registration_email(email, confirmation_token)
